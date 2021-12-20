b1f7e93abcf2c10954f9e4cd9425e6cc2acc2c0b
Balloonieee <89361915+Balloonieee@users.noreply.github.com>
2021-12-20 14:24:52 -0500

    Update input.js

--- scripts/classes/interaction/input.js
+++ scripts/classes/interaction/input.js
@@ -2,4 +2,24 @@ export default class inputInteraction {
   constructor(input) {
     this.input = input
   }
+  
+  getName() {
+    return this.input?.name
+  }
+  
+  getDescription() {
+    return this.input?.description
+  }
+  
+  getRequired() {
+    return this.input?.required
+  }
+  
+  getType() {
+    return this.input?.type
+  }
+  
+  getValue() {
+    return this.input?.value
+  }
 }
