b0725485c48edae18f81f7ddb83cd62789298135
Balloonieee <89361915+Balloonieee@users.noreply.github.com>
2021-12-20 13:25:35 -0500

    Update command.js

--- scripts/classes/interaction/command.js
+++ scripts/classes/interaction/command.js
@@ -1,1 +1,42 @@
-
+export default class commandInteraction {
+  constructor(command) {
+    this.command = command
+  }
+  
+  getName() {
+    return this.command?.name
+  }
+  
+  getDescription() {
+    return this.command?.description
+  }
+  
+  getAliases() {
+    return this.command?.aliases
+  }
+  
+  getUsage() {
+    return this.command?.usage
+  }
+  
+  getRequiredTags() {
+    return this.command?.requiredTags
+  }
+  
+  getInput(name) {
+    const input = this.command?.inputs.find(input => input.name == name)
+    return new inputInteraction(input)
+  }
+  
+  getRequiredInputs() {}
+  
+  getFilledInputs() {}
+  
+  getNonFilledInputs() {}
+  
+  getGroup() {}
+  
+  getRanGroup() {}
+  
+  getNonRanGroups
+}
