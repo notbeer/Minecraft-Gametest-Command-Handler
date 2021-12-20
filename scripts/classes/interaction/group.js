880acce9bc7b9bac1dfe97246bc1a11c99109edd
Balloonieee <89361915+Balloonieee@users.noreply.github.com>
2021-12-20 14:41:20 -0500

    Update group.js

--- scripts/classes/interaction/group.js
+++ scripts/classes/interaction/group.js
@@ -1,1 +1,32 @@
+import inputInteraction from './input.js'
 
+class groupInteraction {
+  constructor(group) {
+    this.group = group
+  }
+  
+  getRan() {
+    return this.group?.ran
+  }
+  
+  getName() {
+    return this.group?.name
+  }
+  
+  getDescription() {
+    retrun this.group?.description
+  }
+  
+  getAliases() {
+    return this.group?.aliases
+  }
+  
+  getUsages() {
+    return this.group?.usages
+  }
+  
+  getInput(name) {
+    const input = this.group?.options?.find(option => option.name == name)
+    return new inputInteraction(input)
+  }
+}
