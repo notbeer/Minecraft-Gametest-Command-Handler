e918a016521cc3d914957a06a5a2d2358c613502
Balloonieee <89361915+Balloonieee@users.noreply.github.com>
2021-12-20 20:50:45 -0500

    Update typeParser.js

--- scripts/utils/typeParser.js
+++ scripts/utils/typeParser.js
@@ -8,18 +8,18 @@ class typeParser {
   
   int(value) {
     const data = +value
-    if(!data) return { error: true, errorMessage: `${value} is not a number!`}
+    if(!data) throw new Error(`${value} is not a number!`} //return { error: true, errorMessage: `${value} is not a number!`}
     return data
   }
   
   boolean(value) {
-    if(value != 'true' && value != 'false') return { error: true, errorMessage: `${value} is not a boolean`}
+    if(value != 'true' && value != 'false') throw new Error(`${value} is not a boolean`}//return { error: true, errorMessage: `${value} is not a boolean`}
     return value === 'true'
   }
   
   player(value) {
     const player = World.getPlayers().find(player => player.name == value || player.nameTag == value)
-    if(!value) return { error: true, errorMessage: `${value} is not a player who is online` }
+    if(!value) throw new Error(`${value} is not a player who is online`) //return { error: true, errorMessage: `${value} is not a player who is online` }
     return player
   }
 } 
