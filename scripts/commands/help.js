ed0465bbeb4c276958a99bcf546ca55c2ae17abe
Balloonieee <89361915+Balloonieee@users.noreply.github.com>
2021-12-20 21:06:33 -0500

    Update help.js

--- scripts/commands/help.js
+++ scripts/commands/help.js
@@ -1,1 +1,14 @@
+import CommandBuilder from "./classes/builders/CommandBuilder";
+const register = new CommandBuilder()
+    .setCancelMessage(true)
+    .setName('help')
+    .setAliases(['h'])
+    .setDescription('get help!')
+    .setUsage(['help', 'help <command name>'])
+    .addInput(input => input.setName('command name').setDescription('name of command you need help on').setType('string'))
 
+<comamd smt>.register(register, (interaction) => {
+  const commandName = interaction.command.getInput('command name')
+  if(!commandName) //send msg
+  //retrive info for command etc
+})
