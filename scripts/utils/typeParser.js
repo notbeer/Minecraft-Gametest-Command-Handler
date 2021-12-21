91faca5369989e9b0ad9f6a90af785eea7b0ee14
Balloonieee <89361915+Balloonieee@users.noreply.github.com>
2021-12-20 19:47:21 -0500

    Update typeParser.js

--- scripts/utils/typeParser.js
+++ scripts/utils/typeParser.js
@@ -1,1 +1,26 @@
+import { World } from 'mojang-minecraft'
+class typeParser {
+  constructor() {}
+  
+  string(value) {
+    return value.toString()
+  }
+  
+  int(value) {
+    const data = +value
+    if(!data) return { error: true, errorMessage: `${value} is not a number!`}
+    return data
+  }
+  
+  boolean(value) {
+    if(value != 'true' && value != 'false') return { error: true, errorMessage: `${value} is not a boolean`}
+    return value === 'true'
+  }
+  
+  player(value) {
+    const player = World.getPlayers().find(player => player.name == value || player.nameTag == value)
+    if(!value) return { error: true, errorMessage: `${value} is not a player who is online` }
+    return player
+  }
+} 
 
