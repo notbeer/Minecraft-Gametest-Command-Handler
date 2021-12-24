4d6985ffa6a9ff6c0c089578ddee575f7c013bb9
Balloonieee <89361915+Balloonieee@users.noreply.github.com>
2021-12-23 20:11:40 -0500

    Update group.js

--- scripts/classes/parser/group.js
+++ scripts/classes/parser/group.js
@@ -15,7 +15,7 @@ export default class CommandGroupsParser {
     for(const ParsedGroup of ParsedGroups) {
       const GroupIndex = ParsedGroups.indexOf(ParsedGroup)
       const GroupInputs = ParsedGroup.inputs
-      if(!GroupInputs) continue;
+      if(!GroupInputs || !ParsedGroup.ran) continue;
       
       this.args.shift()
       let ParsedGroupInputs = new CommandInputsParser({ inputs: GroupInputs, args: this.args )
