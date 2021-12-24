89c15dd4e2bd9ed8324b36fdf7adcaf4d225e45e
Balloonieee <89361915+Balloonieee@users.noreply.github.com>
2021-12-23 21:44:36 -0500

    Update input.js

--- scripts/classes/parser/input.js
+++ scripts/classes/parser/input.js
@@ -10,7 +10,38 @@ export default class CommandInputsParser {
     const ParsedInputs = []
     
     for(const Input of this.inputs) {
-      let InputIndex = this.inputs.indexOf(Input)
+      const InputIndex = this.inputs.indexOf(Input)
+      let playerInput = this.args[index]
+      
+      if(Input.required && !playerInput)
+        throw new Error(`input for ${Input.name} is required!`)
+     
+      try {
+        switch(Input.type) {
+          case "string":
+            if(!playerInput) break;
+            playerInput = CommandTypeParser.toString(playerInput)
+            break;
+          case "int":
+            if(!playerInput) break;
+            playerInput = CommandTypeParser.toInt(playerInput)
+            break;
+          case "boolean":
+            if(!playerInput) break;
+            playerInput = CommandTypeParser.toBoolean(playerInput)
+            break;
+          case "player":
+            if(!playerInput) break;
+            playerInput = CommandTypeParser.toPlayer(playerInput)
+            break;
+          default:
+            break;
+        }
+      } catch(e) {
+        throw new Error(`incorrect input type for ${Input.name}, input type must be a/an ${Input.type}`)
+      }
+      
+      
     }
     return ParsedInputs
   }
