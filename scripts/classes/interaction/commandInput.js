3337625525abacdc1e0d9f86ade6d88645711697
Balloonieee <89361915+Balloonieee@users.noreply.github.com>
2021-12-23 23:36:40 -0500

    Update and rename input.js to commandInput.js

---
+++ scripts/classes/interaction/commandInput.js
@@ -0,0 +1,49 @@
+export default class CommandInputInteraction {
+  constructor(input) {
+    this.input = input
+  }
+  
+  getName() {
+    return this.input?.name
+  }
+  
+  getDescription() {
+    return this.input?.description
+  }
+  
+  getRequired() {
+    return this.input?.required
+  }
+  
+  getType() {
+    return this.input?.playerInput
+  }
+  
+  getValue() {
+    return this.input?.playerInput
+  }
+} {
+  constructor(input) {
+    this.input = input
+  }
+  
+  getName() {
+    return this.input?.name
+  }
+  
+  getDescription() {
+    return this.input?.description
+  }
+  
+  getRequired() {
+    return this.input?.required
+  }
+  
+  getType() {
+    return this.input?.playerInput
+  }
+  
+  getValue() {
+    return this.input?.playerInput
+  }
+}
