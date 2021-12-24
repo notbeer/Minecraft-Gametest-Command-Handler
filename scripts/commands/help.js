523d600dcbbefd4785fcc1b046c149abb1c8341a
Balloonieee <89361915+Balloonieee@users.noreply.github.com>
2021-12-24 02:28:52 -0500

    Create help.js

---
+++ scripts/commands/help.js
@@ -0,0 +1,25 @@
+import { World, Commands } from "mojang-minecraft"
+import CommandBuilder from "../classes/builders/CommandBuilder.js";
+import CommandHandler from "../classes/CommandRegistration.js"
+
+const registration = new CommandBuilder()
+.setName('test')
+.setAliases(['t'])
+.setDescription('testing to see if the command handler works!')
+.setUsage(['this is a test!'])
+.setCancelMessage(false)
+.addInput(input => {
+  return input.setName('test1').setRequired(true).setType('string')
+})
+.addGroup(group => {
+  return group.setName('test2')
+  .addInput(input => {
+    return input.setName('test21').setRequired(true).setType('any')
+  })
+})
+
+CommandHandler.register(registration, (interaction) => {
+  Commands.run(`say ree:\n ${JSON.stringify(interaction?.command)}\n\n${JSON.stringify(interaction)}`, World.getDimension("overworld"))
+  const input = interaction.command.getInput('test1')
+  Commands.run("say this actually works! wow! input value" + input, World.getDimension("overworld"))
+})
