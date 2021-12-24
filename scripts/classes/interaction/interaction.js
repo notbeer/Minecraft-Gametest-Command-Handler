b5d06d400a88729caaa3acf93419acbc1b73b867
Balloonieee <89361915+Balloonieee@users.noreply.github.com>
2021-12-23 23:41:38 -0500

    Update interaction.js

--- scripts/classes/interaction/interaction.js
+++ scripts/classes/interaction/interaction.js
@@ -1,1 +1,10 @@
+class Interaction {
+  constructor(command, player, message, args) {
+    this.command = new CommandInteraction(command)
+    this.player = player
+    this.args = args
+    this.message = message
+  }
+}
 
+  
