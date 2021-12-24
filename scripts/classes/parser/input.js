699e0d628141b84d65655f973e41149ebf49c93b
Balloonieee <89361915+Balloonieee@users.noreply.github.com>
2021-12-23 19:45:26 -0500

    Update input.js

--- scripts/classes/parser/input.js
+++ scripts/classes/parser/input.js
@@ -1,3 +1,11 @@
 export default class CommandInputsParser {
+  constructor({ inputs, args }) {
+    this.inputs = inputs
+    this.args = args
+  }
   
+  toParsedInputs() {
+    
+    return ParsedInputs
+  }
 }
