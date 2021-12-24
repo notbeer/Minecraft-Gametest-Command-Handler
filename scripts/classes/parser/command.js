ca0bccdd86933846ab8943eb1cf2ea5190beed7d
Balloonieee <89361915+Balloonieee@users.noreply.github.com>
2021-12-23 19:37:07 -0500

    Update command.js

--- scripts/classes/parser/command.js
+++ scripts/classes/parser/command.js
@@ -1,9 +1,12 @@
 class CommandParser {
   constructor({ command, args }) {
-    
+    this.command = command
+    this.args = args
   }
   
   toParsedCommand() {
+    let ParsedCommand;
     
+    return ParsedCommand
   }
 }
