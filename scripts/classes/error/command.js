17314c6cc9818e1c08888d763ddf9497d5ad5738
Balloonieee <89361915+Balloonieee@users.noreply.github.com>
2021-12-23 22:16:00 -0500

    Update command.js

--- scripts/classes/error/command.js
+++ scripts/classes/error/command.js
@@ -1,1 +1,16 @@
+import { Commands, World } from 'mojang-minecraft'
 
+export default class CommandError {
+  constructor({ message, player }) {
+    this.message = message;
+    this.player = player;
+    this.sendErrorMessage();
+  }
+  
+  sendErrorMessage() {
+    const name = this.player.nameTag;
+    const command = `tellraw "${name}" ${JSON.stringify({ rawtext: [ { text: this.message } ] })}`
+    Commands.run(command, World.getDimension('overworld'))
+    //return new Error(`this.message at command: ${this.command?.name}`)
+  }
+}
