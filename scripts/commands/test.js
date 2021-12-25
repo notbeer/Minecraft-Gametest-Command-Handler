b030287d8ce0e2c5acae514e3b9fa9ea1004e0c7
Balloonieee <89361915+Balloonieee@users.noreply.github.com>
2021-12-25 18:55:45 -0500

    Update test.js

--- scripts/commands/test.js
+++ scripts/commands/test.js
@@ -19,7 +19,7 @@ const registration = new CommandBuilder()
 })
 
 CommandHandler.register(registration, (interaction) => {
-  Commands.run(`say ree:\n ${JSON.stringify(interaction?.command)}\n\n${JSON.stringify(interaction)}`, World.getDimension("overworld"))
-  const input = interaction.command.getInput('test1').getValue()
-  Commands.run("say this actually works! wow! input value" + input, World.getDimension("overworld"))
+  const input = interaction.command?.getInput('test1')?.getValue()
+  const groupInput = interaction.command?.getGroup('test2')?.getInput('test21')?.getValue()
+  Commands.run(`say :\ninput test2: ${input}\ninputGroup: ${groupInput}` + input, World.getDimension("overworld"))
 })
