aca316ed87a9df37f959824d0140d09fc09ef7e8
Balloonieee <89361915+Balloonieee@users.noreply.github.com>
2021-12-27 17:36:24 -0500

    Update prefix.js

--- scripts/commands/prefix.js
+++ scripts/commands/prefix.js
@@ -1,1 +1,20 @@
 
+import { World, Commands } from "mojang-minecraft"
+import CommandBuilder from "../classes/builders/CommandBuilder.js";
+import CommandHandler from "../classes/CommandRegistration.js"
+
+const registration = new CommandBuilder()
+.setName('prefix')
+.setAliases(['p'])
+.setDescription('manage the command prefix!')
+.setUsage(['prefix', 'prefix set <new prefix: string>'])
+.setCancelMessage(true)
+.setPrivate(false)
+.addGroup(group => {
+  return group.setName('set').setAliases(['change']).setDescription('change the prefix').addInput(input => {
+    return input.setRequired(true).setType('string').setName('new prefix')
+  })
+})
+
+CommandHandler.register(registration, (interaction) => {
+})
