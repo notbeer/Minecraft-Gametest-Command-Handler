65145a891e3015afca34561afa98b6b449bb17d0
Balloonieee <89361915+Balloonieee@users.noreply.github.com>
2021-12-28 00:27:19 -0500

    Delete src directory

--- src/utils/validator.ts
+++
@@ -1,7 +0,0 @@
-export function validateBoolean(value: any): void {
-    if(typeof value !== 'boolean') throw new Error('Value must be a boolean.');
-};
-export function validateString(value: any, regex?: boolean) {
-    if(typeof value !== 'string') throw new Error('Value must be a string.');
-    if(regex && !/^[^A-Z\s]+$/.test(value)) throw new Error('Value does NOT match the expression "/^[^A-Z]+$/".');
-};
